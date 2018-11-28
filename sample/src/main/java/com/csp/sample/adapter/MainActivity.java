472960fc09f8ba692cc022e40fbf4bfe9ab2d245
jefshi <jefshi@yeah.net>
2018-11-28 16:08:15 +0800

    升级版本

--- sample/src/main/java/com/csp/sample/adapter/MainActivity.java
+++ sample/src/main/java/com/csp/sample/adapter/MainActivity.java
@@ -2,6 +2,7 @@ package com.csp.sample.adapter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rcvSingle = findViewById(R.id.rcv_single);
+        RecyclerView rcvMultiple = findViewById(R.id.rcv_multiple);
+
+
     }
 }
